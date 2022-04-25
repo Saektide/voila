caef39092927a4352c0a4c3ebeed2bd8130a2210
saektide <gh.sktd@protonmail.com>
2022-04-25 03:35:52 -0300

    style: improve SettingsCheckbox feedback

--- components/Settings/Checkbox.vue
+++ components/Settings/Checkbox.vue
@@ -1,11 +1,10 @@
 <template>
   <label
-    class="text-mono-3 rounded-xl py-3 px-5 border w-full flex cursor-pointer duration-100 select-none"
-    :class="{
-      'bg-mono-9 hover:bg-mono-8 border-mono-8': isChecked,
-      'border-mono-9 hover:bg-mono-9': !isChecked
-    }"
+    class="group text-mono-6 border-b border-mono-9 dark:border-mono-3 dark:text-mono-8 font-bold py-5 w-full flex cursor-pointer duration-100 select-none items-center justify-between"
     :for="`settings-${id}`"
+    @mousedown="handleMouseEvent"
+    @mouseup="handleMouseEvent"
+    @mouseleave="handleMouseEvent"
   >
     <input
       :id="`settings-${id}`"
@@ -18,6 +17,26 @@
     <span class="pointer-events-none">
       <slot />
     </span>
+    <div class="pointer-events-none">
+      <div
+        class="rounded-full w-10 flex items-center h-6 relative px-1"
+        :class="{
+          'bg-mono-7': !isChecked,
+          'bg-safety-green': isChecked
+        }"
+      >
+        <div
+          class="h-4 bg-mono-12 shadow rounded-full duration-200 absolute"
+          :class="{
+            'left-1': !isChecked,
+            'right-1 left-5': isChecked && !isActive,
+            'right-1 left-4': isChecked && isActive,
+            'w-4': !isActive,
+            'w-5': isActive
+          }"
+        />
+      </div>
+    </div>
   </label>
 </template>
 
@@ -40,6 +59,11 @@ export default {
       default: 'settings'
     }
   },
+  data() {
+    return {
+      isActive: false
+    }
+  },
   computed: {
     ...mapGetters({
       appSettings: 'settings/appSettings'
@@ -58,6 +82,14 @@ export default {
         settingName: name.split('settings-')[1],
         value: checked
       })
+    },
+    handleMouseEvent (event) {
+      const eventActions = {
+        mousedown: () => { this.isActive = true },
+        mouseup: () => { this.isActive = false },
+        mouseleave: () => { if (this.isActive) { this.isActive = false } }
+      }
+      return eventActions[event.type]()
     }
   }
 }
