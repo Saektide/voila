ac512556783e6b3b9a0665479904554cfa346ea8
saektide <gh.sktd@protonmail.com>
2022-05-12 03:37:06 -0300

    chore: improve Checkboxes

--- components/Checkbox.vue
+++ components/Checkbox.vue
@@ -1,16 +1,16 @@
 <template>
   <label
     class="group text-mono-6 border-b border-mono-9 dark:border-mono-3 dark:text-mono-8 font-bold py-5 w-full flex cursor-pointer duration-100 select-none items-center justify-between"
-    :for="`va-settings-${id}`"
+    :for="`va-${id}`"
     @mousedown="handleMouseEvent"
     @mouseup="handleMouseEvent"
     @mouseleave="handleMouseEvent"
   >
     <input
-      :id="`va-settings-${id}`"
+      :id="`va-${id}`"
       type="checkbox"
-      :name="`settings-${name}`"
-      :value="isChecked"
+      :name="`va-${name}`"
+      :value="checked"
       hidden
       :disabled="disabled"
       @change="handleCheckbox"
@@ -22,17 +22,17 @@
       <div
         class="rounded-full w-10 flex items-center h-6 relative px-1 duration-200"
         :class="{
-          'bg-mono-7': !isChecked,
-          'bg-blue-4': isChecked,
+          'bg-mono-7': !checked,
+          'bg-blue-4': checked,
           'opacity-50 cursor-not-allowed': disabled
         }"
       >
         <div
           class="h-4 bg-mono-12 shadow rounded-full duration-200 absolute inline-flex overflow-hidden"
           :class="{
-            'left-1': !isChecked,
-            'right-1 left-5': isChecked && !isActive,
-            'right-1 left-4': isChecked && isActive,
+            'left-1': !checked,
+            'right-1 left-5': checked && !isActive,
+            'right-1 left-4': checked && isActive,
             'w-4': !isActive,
             'w-5': isActive
           }"
@@ -41,14 +41,14 @@
             class="w-4 text-mono-7 transform absolute duration-300"
             :class="{
               'translate-x-1/3': isActive,
-              'translate-x-full': isChecked
+              'translate-x-full': checked
             }"
           />
           <solid-check-icon
             class="w-4 text-blue-4 transform absolute duration-300"
             :class="{
               '-translate-x-1/3': isActive,
-              '-translate-x-full': !isChecked
+              '-translate-x-full': !checked
             }"
           />
         </div>
@@ -58,10 +58,8 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from 'vuex'
-
 export default {
-  name: 'SettingsCheckbox',
+  name: 'AppCheckbox',
   props: {
     name: {
       type: String,
@@ -71,9 +69,13 @@ export default {
       type: String,
       default: ''
     },
-    sector: {
-      type: String,
-      default: 'settings'
+    checked: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   },
   data() {
@@ -81,25 +83,13 @@ export default {
       isActive: false
     }
   },
-  computed: {
-    ...mapGetters({
-      appSettings: 'settings/appSettings'
-    }),
-    isChecked () {
-      return this.appSettings[this.name]
-    }
-  },
+  computed: {},
   methods: {
-    ...mapMutations({
-      setSetting: 'settings/setSetting'
-    }),
     handleCheckbox (event) {
       const { name } = event.target
-      const checked = this.isChecked
-      this.setSetting({
-        settingName: name.split('settings-')[1],
-        value: !checked
-      })
+      const checked = this.checked
+      const emitterName = !checked ? 'check' : 'uncheck'
+      this.$emit(emitterName, { name, checked })
     },
     handleMouseEvent (event) {
       const eventActions = {
